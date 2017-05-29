cd3f96983134075a93d2f911978d3517d06c476a
fearsomesole <makelamikael1@gmail.com>
2017-05-29 21:18:07 +0300

    Created the first version
    first things that were done.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace LonelyComputerProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Initialization();
+			mainMenu();
+		}
+
+		static void mainMenu()
+		{
+			Console.WriteLine("1. ");
+			Console.WriteLine("2. ");
+			Console.WriteLine("3. ");
+			Console.WriteLine("4. Exit");
+		Start:
+			int userPick = Convert.ToInt16(Console.ReadLine());
+			switch (userPick)
+			{
+				case 1:
+					case1();
+					break;
+				case 2:
+					case2();
+					break;
+				case 3:
+					case3();
+					break;
+				case 4:
+					exit();
+					break;
+				default:
+					SlowWriter.Write("I am sorry, but I do not have those commands. Would you try one of the upper ones?");
+					goto Start;
+			}
+		}
+
+		static void Initialization()
+		{
+			Console.Clear();
+			string username = Environment.UserName;
+			SlowWriter.Write("Hello, " + username + ", how can I help you?\n \n");
+		}
+
+		static void case1()
+		{
+			Console.WriteLine("you chose first case \nTry again?\n");
+			mainMenu();
+		}
+
+		static void case2()
+		{
+			Console.WriteLine("you chose second case \nTry again?\n");
+			mainMenu();
+		}
+		static void case3()
+		{
+			Console.WriteLine("you chose third case \nTry again?\n");
+			mainMenu();
+		}
+		static void exit()
+		{
+			SlowWriter.Write("Are you sure you want to leave me? \ny/n\n");
+			Console.ReadLine();
+		}//hello
+
+	}
+}
---
+++ SlowWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace LonelyComputerProgram
+{
+	public class SlowWriter
+	{
+		public static void Write(string text)
+		{
+			Random rnd = new Random();
+			foreach (char c in text)
+			{
+				Console.Write(c);
+				Thread.Sleep(rnd.Next(40, 60));
+
+			}
+		}
+	}
+}
